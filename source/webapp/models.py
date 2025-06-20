234c6fe8ed65956a762584c26f69633b726472f3
Kirill Grekhovodov <kirill@wildlabs.co>
2025-06-20 20:04:24 +0600

    создал модель статьи и миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название', null=False, blank=False)
+    content = models.TextField(verbose_name='Контент')
+    author = models.CharField(max_length=50, verbose_name='Автор', null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
+
+
+    class Meta:
+        db_table = 'articles'
+        verbose_name = 'Статья'
+        verbose_name_plural = "Статьи"
