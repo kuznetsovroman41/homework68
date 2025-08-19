017510e936eed4dee6a5a658a84a508fc196de3e
kuznetsovroman41 <romank04052006@gmail.com>
2025-08-19 16:44:22 +0600

    убрал postgresql

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': env.db(),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#   'default': env.db(),
+# }
 
 
 # Password validation
